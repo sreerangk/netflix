eb9bb6cb59538508454d92b24c7c47b7e3acb488
sreerang k <sreerangk77@gmail.com>
2022-12-06 12:19:42 +0530

    add movie api key and get youtube vidos

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
+import { API_KEY,imageUrl } from "../../constants/constants"; 
+import axios from '../../axios'
 import './Banner.css'
 
 function Banner() {
+  let random = Math.floor((Math.random()+0)*19)
+      const  [movie, setMovie] = useState()
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data.results[0])
+      setMovie(response.data.results[random])
+    })
+  }, [])
+  
   return (
-    <div className="banner">
+    <div
+    style={{backgroundImage: `url(${movie ? imageUrl+movie.backdrop_path : "" })`}}
+     className="banner">
       <div className="content">
-        <h1 className="title">Movie Name</h1>
+        <h1 className="title">{movie ? movie.title : ""}</h1>
         <div className="banner_buttons">
           <button className="button">Play</button>
           <button className="button">My List</button>
         </div>
-        <h1 className="description">In publishing and graphic design ,Lorem is a pkacenkder text commonly used to demonstrate the</h1>
+        <h1 className="description">{movie ? movie.overview: ""}</h1>
       </div>
       <div className="fade_bottom"></div>
 
