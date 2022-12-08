4558ee10532ce526198a13a5d2ae2869ef97997e
sreerang k <sreerangk77@gmail.com>
2022-12-08 09:14:48 +0530

    upcoming toprated movies  added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
-import {action,originals } from './urls'
+import {action,originals,trending,upcoming,topRated } from './urls'
 import "./App.css";
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
@@ -12,6 +12,9 @@ function App() {
       <Banner />
       <RowPost url={originals} title='Netflix Originals'/>
       <RowPost url={action} title='Action' isSmall />
+      <RowPost url={trending} title='Trending Now' isSmall />
+      <RowPost url={upcoming} title='Upcoming' isSmall />
+      <RowPost url={topRated} title='topRated' isSmall />
      
     </div>
   );
